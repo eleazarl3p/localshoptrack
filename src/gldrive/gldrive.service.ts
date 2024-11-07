02a1e321ce3751bc893fc3245855ab2415dbc239
egeneste <98227174+eleazarl3p@users.noreply.github.com>
2024-11-07 08:58:18 -0500

    abc

---
+++ src/gldrive/gldrive.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@nestjs/common';
+
+import { google } from 'googleapis';
+import { Readable } from 'stream';
+import { Express } from 'express';
+// import * as fs from 'fs';
+@Injectable()
+export class GldriveService {
+  private driveClient;
+
+  constructor() {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+      scopes: ['https://www.googleapis.com/auth/drive.file'],
+    });
+    this.driveClient = google.drive({ version: 'v3', auth });
+  }
+
+  //   async uploadFile(file: Express.Multer.File) {
+  //     const fileMetadata = {
+  //       name: file.originalname,
+  //       parents: ['1qN4pAUf-jkR0xzhbDTDbAzDtxxb8INjx'], // Optional: Set the folder where you want to save the file
+  //     };
+
+  //     const media = {
+  //       mimeType: file.mimetype,
+  //       body: Readable.from(file.buffer),
+  //     };
+
+  //     const response = await this.driveClient.files.create({
+  //       requestBody: fileMetadata,
+  //       media: media,
+  //       fields: 'id, name',
+  //     });
+
+  //     return response.data;
+  //   }
+
+  async uploadFiles(files: Express.Multer.File[]) {
+    const uploadedFiles = [];
+
+    for (const file of files) {
+      const fileMetadata = {
+        name: file.originalname,
+        parents: ['1qN4pAUf-jkR0xzhbDTDbAzDtxxb8INjx'], // Optional: specify folder ID
+      };
+
+      const media = {
+        mimeType: file.mimetype,
+        body: Readable.from(file.buffer), // Convert buffer to readable stream
+      };
+
+      const response = await this.driveClient.files.create({
+        requestBody: fileMetadata,
+        media: media,
+        fields: 'id, name',
+      });
+
+      uploadedFiles.push(response.data); // Store uploaded file info
+    }
+
+    return uploadedFiles; // Return array of uploaded file data
+  }
+
+  async deleteFile(fileId: string) {
+    await this.driveClient.files.delete({ fileId });
+  }
+
+  async uploadPdfToDrive(pdfBuffer: Buffer, filename: string) {
+    const fileMetadata = {
+      name: filename,
+      parents: ['1qN4pAUf-jkR0xzhbDTDbAzDtxxb8INjx'],
+    };
+
+    const media = {
+      mimeType: 'application/pdf',
+      body: this.bufferToStream(pdfBuffer),
+    };
+
+    const response = await this.driveClient.files.create({
+      requestBody: fileMetadata,
+      media: media,
+      fields: 'id, name',
+    });
+
+    console.log('File uploaded successfully with ID:', response.data.id);
+  }
+
+  bufferToStream(buffer: Buffer): Readable {
+    const readable = new Readable();
+    readable.push(buffer);
+    readable.push(null); // Signal the end of the stream
+    return readable;
+  }
+}
